eda119dca23ea3ef7d336cdc4e02b006d326d604
kaiky-yun <yunkaikyyun@gmail.com>
2026-05-29 18:27:15 -0300

    chore: remover sistema de autenticação completo (#190 #191 #192)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { AuthProvider } from './app/providers/AuthProvider'
 import { AppRouter } from './app/router'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <AppRouter />
-    </AuthProvider>
+    <AppRouter />
   </React.StrictMode>
 )
