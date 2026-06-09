7fa1cf953d89b93c71baf52ccfb3506bb578e983
Caio Martins <caioflmjr@gmail.com>
2026-06-09 20:58:15 -0300

    chore(backend): cria tabela periodo_fase com campos de rito e turnos

--- backend/src/domain/entities/__init__.py
+++ backend/src/domain/entities/__init__.py
@@ -1,8 +1,10 @@
 from domain.entities.baseline_tramitacao import BaselineTramitacao
+from domain.entities.cobertura_snapshot import CoberturaSnapshot
 from domain.entities.evento_tramitacao import EventoTramitacao
 from domain.entities.fase_analitica import FaseAnalitica
 from domain.entities.fase_codigo import FaseCodigo
 from domain.entities.orgao_legislativo import OrgaoLegislativo
+from domain.entities.periodo_fase import PeriodoFase
 from domain.entities.proposicao import Proposicao
 from domain.entities.tipo_evento import TipoEvento
 
@@ -14,4 +16,6 @@ __all__ = [
     "OrgaoLegislativo",
     "Proposicao",
     "TipoEvento",
+    "PeriodoFase",
+    "CoberturaSnapshot",
 ]
